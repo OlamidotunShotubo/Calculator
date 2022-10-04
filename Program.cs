3a08492e9765b29feb66e3fb79c32151c5a0ae4c
Dotun <pilotdot2020@gmail.com>
2022-10-04 17:35:44 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Input the Calculation");
+var input = Console.ReadLine();
+var elements = input.Split(' ');
+var num1 = int.Parse(elements[0]);
+var op = elements[1];
+var num2 = int.Parse(elements[2]);
+var result =0; 
+var elements1 = input.Split('c');
+while(elements1 == "c")
+if (op == "+") {
+    result = num1 + num2;
+}
+if (op == "-") {
+    result = num1 - num2;
+}
+if (op == "*") {
+    result = num1 * num2;
+}
+if (op == "/") {
+    result = num1 / num2;
+}
+Console.WriteLine(result);
+Console.WriteLine("If you wish to continue type (C)");
+Console.Read();
